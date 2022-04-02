93c9da2ea5c8c498e3f0065d3ef5221b70cefd39
Emre <emrekurtt1655@gmail.com>
2022-04-02 21:33:24 +0200

    fix post handler

--- controllers/postCtrl.ts
+++ controllers/postCtrl.ts
@@ -52,12 +52,12 @@ const postCtrl = {
       const { id } = decoded;
       if (!id)
         return res.status(400).json({ message: "Invalid Token Please Login" });
-      const { text, postTopicId, userId, image } = req.body;
+      const { text, postTopicId, postUserId, image } = req.body;
       await prisma.post.create({
         data: {
           postId: `${text.slice(0, 20).replace(/\s+/g, '') + new Date().getMilliseconds() * 3}`,
           text: text,
-          postUserId: userId,
+          postUserId: postUserId,
           postTopicId: postTopicId,
           image: image
         },
